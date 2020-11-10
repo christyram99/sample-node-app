e629264e57be04f45d50f66c141f209be63735ce
christyram99 <christyram99@gmail.com>
2020-11-10 20:07:42 +0530

    removed unwanted functions

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,16 +2,9 @@ import express from 'express'
 import authMiddleware from './../middlewares/authMiddleware'
 import {
   signIn,
-  profile,
-  updateProfile,
-  listAllUsers,
-  updatePassword,
-  forgotPassword,
-  resetPassword,
-  updatePhone,
-  resetPhone,
-  updateUserProfilePic,
+  signUp,
 } from './../controllers/userController'
+
 import schemaValidator from './../middlewares/validations/schemaValidator'
 import fileUploadMiddleware from './../middlewares/fileUploadMiddleware'
 import customValidations from './../middlewares/customValidationsMiddleware'
@@ -23,86 +16,14 @@ router.post('/sign-in',
   [
     validateRequest
   ],
-  signIn
+  signIn,
 )
 
-router.get('/profile',
+router.post('/sign-up',
   [
-    authMiddleware.checkAuthHeader,
-    authMiddleware.validateAccessToken
-  ],
-  profile,
-)
-
-router.patch('/profile',
-  [
-    authMiddleware.checkAuthHeader,
-    authMiddleware.validateAccessToken,
     validateRequest
   ],
-  updateProfile,
-)
-
-router.get('/users', [
-  authMiddleware.checkAuthHeader,
-  authMiddleware.validateAccessToken
-],
-listAllUsers,
-)
-
-router.post('/password/update',
-  [
-    authMiddleware.checkAuthHeader,
-    authMiddleware.validateAccessToken,
-    validateRequest,
-    customValidations.checkCurrentPassword
-  ],
-  updatePassword,
-)
-
-router.post('/password/forgot',
-  [
-    validateRequest,
-    authMiddleware.isUserExistByPhone
-  ],
-  forgotPassword,
-)
-
-router.post('/password/reset',
-  [
-    validateRequest,
-    authMiddleware.isUserExistByPhone
-  ],
-  resetPassword,
-)
-
-router.post('/phone/update',
-  [
-    authMiddleware.checkAuthHeader,
-    authMiddleware.validateAccessToken,
-    validateRequest,
-    customValidations.checkPhoneNumberExist
-  ],
-  updatePhone,
-)
-
-router.post('/phone/reset',
-  [
-    authMiddleware.checkAuthHeader,
-    authMiddleware.validateAccessToken,
-    validateRequest,
-    customValidations.checkPhoneNumberExist
-  ],
-  resetPhone,
-)
-
-router.post('/profile-pic',
-  [
-    authMiddleware.checkAuthHeader,
-    authMiddleware.validateAccessToken,
-    fileUploadMiddleware.saveUserProfilePic,
-  ],
-  updateUserProfilePic,
+  signUp,
 )
 
 export default router
--- src/services/database/userDataServiceProvider.js
+++ src/services/database/userDataServiceProvider.js
@@ -15,53 +15,6 @@ class UserDataServiceProvider {
     if (userDetails) { match = await bcrypt.compare(password, userDetails.password) }
     return match ? userDetails : null
   }
-
-  async getAllUsers (query = {}, skip = 0, limit = 10, sort = {}) {
-    return UserModel.find(query).skip(skip).limit(limit).sort(sort).select({ password: 0 })
-  }
-
-  async countAllUsers (query = {}) {
-    return UserModel.countDocuments(query)
-  }
-
-  async getUserByUsername (username = '') {
-    return UserModel.findOne({ username: username })
-  }
-
-  async getUserById (userId = '') {
-    return UserModel.findById(userId)
-  }
-
-  async getUserByPhone (phoneNumber = '') {
-    return UserModel.findOne({ phone_number: phoneNumber })
-  }
-
-  async updateUserById (userId, userData) {
-    return UserModel.findByIdAndUpdate(userId, { $set: userData }, { new: true })
-  }
-
-  async deleteUserById (userId) {
-    return UserModel.updateOne({ _id: userId }, { $set: { status: 'DELETED' } })
-  }
-
-  async updateUserPasswordById (userId, password) {
-    const updatedPassword = await bcrypt.hash(password, saltRounds)
-    return UserModel.updateOne({ _id: userId }, { $set: { password: updatedPassword } })
-  }
-
-  async updateUserProfilePicById (userId, profilePicPath) {
-    return await UserModel.updateOne({ _id: userId }, { $set: { avatar: profilePicPath } })
-  }
-
-  async checkCurrentPassword (userId, password) {
-    const user = await UserModel.findById(userId)
-
-    return await bcrypt.compare(password, user.password)
-  }
-
-  async checkPhoneExistence (userId, phoneNumber) {
-    return UserModel.findOne({ phone_number: phoneNumber, _id: { $ne: userId } })
-  }
 }
 
 export default new UserDataServiceProvider()
