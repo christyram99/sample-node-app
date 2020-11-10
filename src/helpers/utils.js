a31af18e83ff199e27e6476d967eb3e5616361e4
christyram99 <christyram99@gmail.com>
2020-11-10 08:07:34 +0530

    updated utils export into es6 format

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,17 +2,26 @@
 String.prototype.capitalize = function () {
   return this.toLowerCase().charAt(0).toUpperCase() + this.toLowerCase().slice(1)
 }
-export default {
-  isBoolean: (value) => {
-    return typeof value === 'boolean'
-  },
-  includes: (source = [], element) => {
-    return source.length && element && source.indexOf(element) > -1
-  },
-  has: (source = {}, key) => {
-    return source.hasOwnProperty(key)
-  },
-  get: (source, key) => {
-    return source[key]
-  },
+
+function isBoolean (value) {
+  return typeof value === 'boolean'
+}
+
+function includes (source = [], element) {
+  return source.length && element && source.indexOf(element) > -1
+}
+
+function has (source = {}, key) {
+  return source.hasOwnProperty(key)
+}
+
+function get (source, key) {
+  return source[key]
+}
+
+module.exports =  {
+  isBoolean,
+  includes,
+  has,
+  get,
 }
