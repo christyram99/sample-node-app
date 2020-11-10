ea7de680c5457bf28960965cfa1b3b8c8fccb78a
christyram99 <christyram99@gmail.com>
2020-11-10 19:38:19 +0530

    updated joi signup data schema

--- src/middlewares/validations/schemas/users/userSignUpDataSchema.js
+++ src/middlewares/validations/schemas/users/userSignUpDataSchema.js
@@ -1,7 +1,9 @@
 import Joi from 'joi'
 
 const UserSignUpSchema = Joi.object().keys({
-  name: Joi.string().required(),
+  first_name: Joi.string().required(),
+  last_name: Joi.string().required(),
+  password: Joi.string().required(),
   email: Joi.string().required(),
   mobile_number: Joi.string().required(),
 })
